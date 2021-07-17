de42037b3687fce22f780ed9bab18db09f3eeca6
vmwhoami <vmwhoami@gmail.com>
2021-07-17 12:10:38 +0300

    Add the form fields

--- app/controllers/scrappers_controller.rb
+++ app/controllers/scrappers_controller.rb
@@ -4,11 +4,19 @@ class ScrappersController < ApplicationController
 
   def scrapp
     profile_url = params[:proz_profile_url]
-    scraped_val = scrapp_page(profile_url)
-    
+    @scraped_values = scrapp_page(profile_url)
+   
   end
 
- 
+  def create
+    @scrapp = Scrapper.new
+
+  end
+  
+  def all
+    @scrapped = Scrapper.all
+  end
+  
   
     
  
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@
 Rails.application.routes.draw do
   root 'scrappers#index'
   post '/scrapper', to: 'scrappers#scrapp', as: 'scrapp'
+  post '/add', to: 'scrappers#create', as: 'scrapp_add'
 end
